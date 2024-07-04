ff5cdced390ec65ff55d607955267762b90bfe61
Rohini-Microsoft <v-rwalunj@microsoft.com>
2024-07-04 14:17:50 +0530

    addded label

--- App/frontend/src/components/SidebarView/SidebarView.tsx
+++ App/frontend/src/components/SidebarView/SidebarView.tsx
@@ -67,7 +67,7 @@ export const SidebarView = (): JSX.Element => {
                 }
 
                 <Stack className={styles.avatarContainer}>
-                    <Avatar color="colorful" name={name} title='User name'/>
+                    <Avatar color="colorful" name={name} aria-label='User name'/>
                 </Stack>
             </Stack>
             <Stack horizontal
