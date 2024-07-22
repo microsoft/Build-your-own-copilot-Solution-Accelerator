842e8a6afa18f8d6bbe2faa5b59bdc44359db868
Roopan P M <v-roopanpm@microsoft.com>
2024-07-22 23:53:14 +0530

    create_ai_hub fix

--- Deployment/scripts/aihub_scripts/create_ai_hub.py
+++ Deployment/scripts/aihub_scripts/create_ai_hub.py
@@ -1,163 +1,121 @@
-#Get Azure Key Vault Client
-key_vault_name = 'kv_to-be-replaced'
+# Get Azure Key Vault Client
+key_vault_name = "pslsc-kv-gzwijqcuyu2ry"
 
-from azure.keyvault.secrets import SecretClient  
-from azure.identity import DefaultAzureCredential  
+from azure.ai.ml import MLClient
+from azure.ai.ml.entities import (
+    Hub,
+    Project,
+    ApiKeyConfiguration,
+    AzureAISearchConnection,
+    AzureOpenAIConnection,
+)
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
+from azure.mgmt import search as Se
+
 
 def get_secrets_from_kv(kv_name, secret_name):
-    
-  # Set the name of the Azure Key Vault  
-  key_vault_name = kv_name 
-    
-  # Create a credential object using the default Azure credentials  
-  credential = DefaultAzureCredential()
+    # Set the name of the Azure Key Vault
+    key_vault_name = kv_name
 
-    # Create a secret client object using the credential and Key Vault name  
-  secret_client = SecretClient(vault_url=f"https://{key_vault_name}.vault.azure.net/", credential=credential)  
-    
-  # Retrieve the secret value  
-  return(secret_client.get_secret(secret_name).value)
+    # Create a credential object using the default Azure credentials
+    credential = DefaultAzureCredential()
 
-print(get_secrets_from_kv(key_vault_name,"AZURE-SEARCH-ENDPOINT"))
+    # Create a secret client object using the credential and Key Vault name
+    secret_client = SecretClient(
+        vault_url=f"https://{key_vault_name}.vault.azure.net/", credential=credential
+    )
+
+    # Retrieve the secret value
+    return secret_client.get_secret(secret_name).value
 
 
-from azure.ai.resources.client import AIClient
-from azure.ai.resources.entities import Project, AIResource
-from azure.identity import DefaultAzureCredential 
+print(get_secrets_from_kv(key_vault_name, "AZURE-SEARCH-ENDPOINT"))
 
-from azure.ai.resources.entities import (
-    AzureOpenAIConnection,
-    AzureAISearchConnection,
-    AzureAIServiceConnection
-)
-
-from azure.ai.ml.entities._credentials import ApiKeyConfiguration
-from azure.ai.ml._restclient.v2023_06_01_preview.models import ConnectionAuthType
-from azure.ai.ml._utils.utils import camel_to_snake
-from azure.ai.ml.entities._credentials import ApiKeyConfiguration
-from azure.core.exceptions import ResourceNotFoundError
-
+# Azure configuration
 
 key_vault_name = 'kv_to-be-replaced'
-subscription_id='subscription_to-be-replaced'
+subscription_id = 'subscription_to-be-replaced'
 resource_group_name = 'rg_to-be-replaced'
 aihub_name = 'ai_hub_' + 'solutionname_to-be-replaced'
-project_name='ai_project_' + 'solutionname_to-be-replaced'
+project_name = 'ai_project_' + 'solutionname_to-be-replaced'
 deployment_name = 'draftsinference-' + 'solutionname_to-be-replaced'
 solutionLocation = 'solutionlocation_to-be-replaced'
 
-open_ai_key = get_secrets_from_kv(key_vault_name,"AZURE-OPENAI-KEY")
-open_ai_res_name = get_secrets_from_kv(key_vault_name,"AZURE-OPENAI-ENDPOINT").replace('https://','').replace('.openai.azure.com','').replace('/','')
-openai_api_version = get_secrets_from_kv(key_vault_name,"AZURE-OPENAI-PREVIEW-API-VERSION")
-
-ai_search_endpoint = get_secrets_from_kv(key_vault_name,"AZURE-SEARCH-ENDPOINT")
-ai_search_res_name = get_secrets_from_kv(key_vault_name,"AZURE-SEARCH-ENDPOINT").replace('https://','').replace('.search.windows.net','').replace('/','')
-ai_search_key = get_secrets_from_kv(key_vault_name,"AZURE-SEARCH-KEY")
-
-
-credential1 = DefaultAzureCredential()
-
-ai_client = AIClient(
-    credential=credential1,
-    subscription_id=subscription_id,
-    resource_group_name=resource_group_name
+# Open AI Details
+open_ai_key = get_secrets_from_kv(key_vault_name, "AZURE-OPENAI-KEY")
+open_ai_res_name = (
+    get_secrets_from_kv(key_vault_name, "AZURE-OPENAI-ENDPOINT")
+    .replace("https://", "")
+    .replace(".openai.azure.com", "")
+    .replace("/", "")
+)
+openai_api_version = get_secrets_from_kv(
+    key_vault_name, "AZURE-OPENAI-PREVIEW-API-VERSION"
 )
 
-created_project = None
-created_resource = None
-# create AI resource
-created_resource = ai_client.ai_resources.begin_create(ai_resource=AIResource(
-    name=aihub_name,
-)).result()
-
-# Create project with above AI resource as parent.
-new_local_project = Project(
-    name=project_name,
-    ai_resource=created_resource.id,
-    description="",
+# Azure Search Details
+ai_search_endpoint = get_secrets_from_kv(key_vault_name, "AZURE-SEARCH-ENDPOINT")
+ai_search_res_name = (
+    get_secrets_from_kv(key_vault_name, "AZURE-SEARCH-ENDPOINT")
+    .replace("https://", "")
+    .replace(".search.windows.net", "")
+    .replace("/", "")
 )
-created_project = ai_client.projects.begin_create(project=new_local_project).result()
-print(created_project.name)
-print(created_project.ai_resource)
+ai_search_key = get_secrets_from_kv(key_vault_name, "AZURE-SEARCH-KEY")
 
+# Credentials
+credential = DefaultAzureCredential()
 
-ai_client = AIClient(
-    credential=credential1,
-    subscription_id=subscription_id,
+# Create the ML CLient
+ml_client = MLClient(
     resource_group_name=resource_group_name,
-    project_name=project_name
+    subscription_id=subscription_id,
+    credential=credential,
 )
 
-# create openai connection
-openai_conn_name = 'Azure_OpenAI'
-cred = ApiKeyConfiguration(key=open_ai_key)
-target = f"https://{open_ai_res_name}.openai.azure.com/"
+# construct a hub
+my_hub = Hub(name=aihub_name, location=solutionLocation, display_name=aihub_name)
 
-local_conn = AzureOpenAIConnection(name="overwrite", credentials=None, target="overwrite")
+created_hub = ml_client.workspaces.begin_create(my_hub).result()
 
-local_conn.name = openai_conn_name
-local_conn.credentials = cred
-local_conn.target = target
-local_conn.api_version = openai_api_version #'2023-07-01-preview'
-local_conn.tags["ResourceId"] = f"/subscriptions/{subscription_id}/resourceGroups/{resource_group_name}/providers/Microsoft.CognitiveServices/accounts/{open_ai_res_name}"
+# construct the project
+my_project = Project(
+    name=project_name,
+    location=solutionLocation,
+    display_name=project_name,
+    hub_id=created_hub.id,
+)
 
-ai_created_conn = ai_client.connections.create_or_update(local_conn)
+created_project = ml_client.workspaces.begin_create(workspace=my_project).result()
 
-print(ai_created_conn.name, ai_created_conn.type, ai_created_conn.target, ai_created_conn.credentials.type)
+open_ai_connection = AzureOpenAIConnection(
+    name=open_ai_res_name,
+    api_key=open_ai_key,
+    api_version=openai_api_version,
+    azure_endpoint=f"https://{open_ai_res_name}.openai.azure.com/",
+    open_ai_resource_id=f"/subscriptions/{subscription_id}/resourceGroups/{resource_group_name}/providers/Microsoft.Search/searchServices/{ai_search_res_name}",
+)
 
+# Create an ML client
+ml_client = MLClient(
+    workspace_name=aihub_name,
+    resource_group_name=resource_group_name,
+    subscription_id=subscription_id,
+    credential=credential,
+)
 
-# create search connection
-search_conn_name = 'Azure_AISearch'
-cred = ApiKeyConfiguration(key=ai_search_key)
-target = ai_search_endpoint  #f"https://{ai_search_res_name}.search.windows.net/"
+ml_client.connections.create_or_update(open_ai_connection)
 
-local_conn = AzureAISearchConnection(name="overwrite", credentials=None, target="overwrite")
-local_conn.name = search_conn_name
-local_conn.credentials = cred
-local_conn.target = target
-local_conn.api_version = "2023-07-01-preview"
-local_conn.tags["ApiType"] = 'Azure'
-local_conn.tags["ApiVersion"] = "2023-07-01-preview"
-local_conn.tags["ResourceId"] = f"/subscriptions/{subscription_id}/resourceGroups/{resource_group_name}/providers/Microsoft.Search/searchServices/{ai_search_res_name}"
+# Create AI Search resource
+search_client = Se.SearchManagementClient(
+    credential=credential, subscription_id=subscription_id
+)
 
+aisearch_connection = AzureAISearchConnection(
+    name=ai_search_res_name,
+    endpoint=ai_search_endpoint,
+    credentials=ApiKeyConfiguration(key=ai_search_key),
+)
 
-srch_created_conn = ai_client.connections.create_or_update(local_conn)
-
-print(srch_created_conn.name, srch_created_conn.type, srch_created_conn.target, srch_created_conn.credentials.type)
-
-ai_client.connections.get(search_conn_name)
-
-# #create deployment endpoint
-# from azure.ai.resources.entities.single_deployment import SingleDeployment
-# from azure.ai.resources.entities.models import PromptflowModel
-# import time 
-
-# if not deployment_name:
-#     deployment_name = f"{ai_client.project_name}-copilot"
-
-# deployment = SingleDeployment(
-#     name=deployment_name,
-#     model=PromptflowModel(
-#         path="./DraftFlow",
-#     ),
-#     instance_type='Standard_DS2_v2',
-#     instance_count=1
-# )
-# ai_client.single_deployments.begin_create_or_update(deployment)
-
-# depl_key = None
-# while True:
-#     try:
-#         depl1_keys = ai_client.single_deployments.get_keys(name=deployment_name,endpoint_name=deployment_name)
-#         if depl1_keys.primary_key != None:
-#             depl_key = depl1_keys.primary_key
-#             depl_endpoint = f'https://{deployment_name}.{solutionLocation}.inference.ml.azure.com/score'
-#             break
-#         else:
-#             print('didnt find the deployment keys, waiting')
-#             time.sleep(120) 
-#     except:
-#         print('didnt find the deployment, waiting')
-#         time.sleep(120)
-
-# print(depl_key,depl_endpoint)
+ml_client.connections.create_or_update(aisearch_connection)
