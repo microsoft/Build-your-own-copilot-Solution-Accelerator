a9d9c41c66557df3ea5990f311c83f04719466b5
pradeepjha-microsoft <v-pradeepjha@microsoft.com>
2024-09-09 12:42:48 +0530

    Fixed the bug-7874

--- ClientAdvisor/AzureFunction/function_app.py
+++ ClientAdvisor/AzureFunction/function_app.py
@@ -295,6 +295,7 @@ async def stream_openai_text(req: Request) -> StreamingResponse:
     Only use the client name returned from database in the response.
     Always consider to give selected client full name only in response and do not use other example names also consider my client means currently selected client.
     If you cannot answer the question, always return - I cannot answer this question from the data available. Please rephrase or add more details.
+    Do not include or reference any client IDs, sensitive data, or personal identifiers in the responses.
     ** Remove any client identifiers or ids or numbers or ClientId in the final response.
 
     '''
