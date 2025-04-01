03d13f14fe43cd79891bd5fb5ce9623406e14e0d
UtkarshMishra-Microsoft <v-utkamishra@microsoft.com>
2025-04-01 20:39:46 +0530

    package and ai-script (#426)

--- ResearchAssistant/Deployment/scripts/aihub_scripts/create_ai_hub.py
+++ ResearchAssistant/Deployment/scripts/aihub_scripts/create_ai_hub.py
@@ -1,7 +1,6 @@
 # Get Azure Key Vault Client
 key_vault_name = 'kv_to-be-replaced'
 
-import time
 from azure.ai.ml import MLClient
 from azure.ai.ml.entities import (
     Hub,
@@ -74,10 +73,7 @@ ml_client = MLClient(
 # construct a hub
 my_hub = Hub(name=aihub_name, location=solutionLocation, display_name=aihub_name)
 
-created_hub = ml_client.workspaces.begin_create(my_hub)  
-   
-time.sleep(60) # wait for hub to be created
-created_hub = ml_client.workspaces.get(name=aihub_name)
+created_hub = ml_client.workspaces.begin_create(my_hub).result()
 
 # construct the project
 my_project = Project(
@@ -87,9 +83,7 @@ my_project = Project(
     hub_id=created_hub.id,
 )
 
-created_project = ml_client.workspaces.begin_create(workspace=my_project)
- 
-time.sleep(30) # wait for hub to be created
+created_project = ml_client.workspaces.begin_create(workspace=my_project).result()
 
 open_ai_connection = AzureOpenAIConnection(
     name="Azure_OpenAI",
