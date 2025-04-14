52cc4009475177d93e2406b15ddf427fc0e598f7
Harsh-Microsoft <v-hbangera@microsoft.com>
2025-04-14 11:20:18 +0530

    updated post deployment scripts such that it can be run via container as well manually

--- infra/scripts/copy_kb_files.sh
+++ infra/scripts/copy_kb_files.sh
@@ -6,32 +6,90 @@ fileSystem="$2"
 baseUrl="$3"
 managedIdentityClientId="$4"
 
+echo "Script Started"
+
+# Authenticate with Azure
+if az account show &> /dev/null; then
+    echo "Already authenticated with Azure."
+else
+    if [ -n "$managedIdentityClientId" ]; then
+        # Use managed identity if running in Azure
+        echo "Authenticating with Managed Identity..."
+        az login --identity --client-id ${managedIdentityClientId}
+    else
+        # Use Azure CLI login if running locally
+        echo "Authenticating with Azure CLI..."
+        az login
+    fi
+    echo "Not authenticated with Azure. Attempting to authenticate..."
+fi
+
+echo "Getting signed in user id"
+signed_user_id=$(az ad signed-in-user show --query id -o tsv)
+
+echo "Getting storage account resource id"
+storage_account_resource_id=$(az storage account show --name $storageAccount --query id --output tsv)
+
+#check if user has the Storage Blob Data Contributor role, add it if not
+echo "Checking if user has the Storage Blob Data Contributor role"
+role_assignment=$(MSYS_NO_PATHCONV=1 az role assignment list --assignee $signed_user_id --role "Storage Blob Data Contributor" --scope $storage_account_resource_id --query "[].roleDefinitionId" -o tsv)
+if [ -z "$role_assignment" ]; then
+    echo "User does not have the Storage Blob Data Contributor role. Assigning the role."
+    MSYS_NO_PATHCONV=1 az role assignment create --assignee $signed_user_id --role "Storage Blob Data Contributor" --scope $storage_account_resource_id --output none
+    if [ $? -eq 0 ]; then
+        echo "Role assignment completed successfully."
+    else
+        echo "Error: Role assignment failed."
+        exit 1
+    fi
+else
+    echo "User already has the Storage Blob Data Contributor role."
+fi
+
 zipFileName1="clientdata.zip"
 extractedFolder1="clientdata"
-zipUrl1=${baseUrl}"infra/data/clientdata.zip"
 
 zipFileName2="clienttranscripts.zip"
 extractedFolder2="clienttranscripts"
-zipUrl2=${baseUrl}"infra/data/clienttranscripts.zip"
 
-# Create folders if they do not exist
-mkdir -p "/mnt/azscripts/azscriptinput/$extractedFolder1"
-mkdir -p "/mnt/azscripts/azscriptinput/$extractedFolder2"
+#check if baseUrl is provided, if not, set it to empty string
+if [ -z "$baseUrl" ]; then
+    baseUrl=""
+fi
 
-# Download the zip file
-curl --output /mnt/azscripts/azscriptinput/"$zipFileName1" "$zipUrl1"
-curl --output /mnt/azscripts/azscriptinput/"$zipFileName2" "$zipUrl2"
+zipUrl1=${baseUrl}"infra/data/$zipFileName1"
+zipUrl2=${baseUrl}"infra/data/$zipFileName2"
 
-# Extract the zip file
-unzip /mnt/azscripts/azscriptinput/"$zipFileName1" -d /mnt/azscripts/azscriptinput/"$extractedFolder1"
-unzip /mnt/azscripts/azscriptinput/"$zipFileName2" -d /mnt/azscripts/azscriptinput/"$extractedFolder2"
+extractionPath1=""
+extractionPath2=""
 
-echo "Script Started"
+# Check if running in Azure Container App
+if !([ -z "$baseUrl" ] && [ -z "$managedIdentityClientId" ]); then
+    $extractionPath1="/mnt/azscripts/azscriptinput/$extractedFolder1"
+    $extractionPath2="/mnt/azscripts/azscriptinput/$extractedFolder2"
 
-# Authenticate with Azure using managed identity
-az login --identity --client-id ${managedIdentityClientId}
+    # Create the folders if they do not exist
+    mkdir -p $extractionsPath1
+    mkdir -p $extractionsPath2
+
+    # Download the zip file
+    curl --output /mnt/azscripts/azscriptinput/"$zipFileName1" "$zipUrl1"
+    curl --output /mnt/azscripts/azscriptinput/"$zipFileName2" "$zipUrl2"
+
+    # Extract the zip file
+    unzip /mnt/azscripts/azscriptinput/"$zipFileName1" -d $extractionPath1
+    unzip /mnt/azscripts/azscriptinput/"$zipFileName2" -d $extractionPath2
+
+else
+    extractionPath1="infra/data/$extractedFolder1"
+    extractionPath2="infra/data/$extractedFolder2"
+
+    unzip -o $zipUrl1 -d $extractionPath1
+    unzip -o $zipUrl2 -d $extractionPath2
+fi
+
 # Using az storage blob upload-batch to upload files with managed identity authentication, as the az storage fs directory upload command is not working with managed identity authentication.
-az storage blob upload-batch --account-name "$storageAccount" --destination data/"$extractedFolder1" --source /mnt/azscripts/azscriptinput/"$extractedFolder1" --auth-mode login --pattern '*' --overwrite
-az storage blob upload-batch --account-name "$storageAccount" --destination data/"$extractedFolder2" --source /mnt/azscripts/azscriptinput/"$extractedFolder2" --auth-mode login --pattern '*' --overwrite
+az storage blob upload-batch --account-name "$storageAccount" --destination data/"$extractedFolder1" --source $extractionPath1 --auth-mode login --pattern '*' --overwrite --output none
+az storage blob upload-batch --account-name "$storageAccount" --destination data/"$extractedFolder2" --source $extractionPath2 --auth-mode login --pattern '*' --overwrite --output none
 # az storage fs directory upload -f "$fileSystem" --account-name "$storageAccount" -s "$extractedFolder1" --account-key "$accountKey" --recursive
 # az storage fs directory upload -f "$fileSystem" --account-name "$storageAccount" -s "$extractedFolder2" --account-key "$accountKey" --recursive
--- infra/scripts/run_create_index_scripts.sh
+++ infra/scripts/run_create_index_scripts.sh
@@ -2,17 +2,80 @@
 echo "started the script"
 
 # Variables
-baseUrl="$1"
-keyvaultName="$2"
+keyvaultName="$1"
+baseUrl="$2"
 managedIdentityClientId="$3"
-requirementFile="requirements.txt"
-requirementFileUrl=${baseUrl}"infra/scripts/index_scripts/requirements.txt"
+resourceGroupName="$4"
+sqlServerName="$5"
 
 echo "Script Started"
 
-# Download the create_index and create table python files
-curl --output "create_search_index.py" ${baseUrl}"infra/scripts/index_scripts/create_search_index.py"
-curl --output "create_sql_tables.py" ${baseUrl}"infra/scripts/index_scripts/create_sql_tables.py"
+# Authenticate with Azure
+if az account show &> /dev/null; then
+    echo "Already authenticated with Azure."
+else
+    if [ -n "$managedIdentityClientId" ]; then
+        # Use managed identity if running in Azure
+        echo "Authenticating with Managed Identity..."
+        az login --identity --client-id ${managedIdentityClientId}
+    else
+        # Use Azure CLI login if running locally
+        echo "Authenticating with Azure CLI..."
+        az login
+    fi
+    echo "Not authenticated with Azure. Attempting to authenticate..."
+fi
+
+# Run the Azure CLI command and store the output
+echo "Getting signed in user id and display name"
+signed_user=$(az ad signed-in-user show --query "{id:id, displayName:displayName}" -o json)
+
+# Extract id and displayName using grep and sed
+signed_user_id=$(echo "$signed_user" | grep -oP '"id":\s*"\K[^"]+')
+signed_user_display_name=$(echo "$signed_user" | grep -oP '"displayName":\s*"\K[^"]+')
+
+# echo "Getting signed in user id"
+# signed_user_id=$(az ad signed-in-user show --query id -o tsv)
+
+echo "Getting key vault resource id"
+key_vault_resource_id=$(az keyvault show --name $keyvaultName --query id --output tsv)
+
+# Check if the user has the Key Vault Administrator role
+echo "Checking if user has the Key Vault Administrator role"
+role_assignment=$(MSYS_NO_PATHCONV=1 az role assignment list --assignee $signed_user_id --role "Key Vault Administrator" --scope $key_vault_resource_id --query "[].roleDefinitionId" -o tsv)
+if [ -z "$role_assignment" ]; then
+    echo "User does not have the Key Vault Administrator role. Assigning the role."
+    MSYS_NO_PATHCONV=1 az role assignment create --assignee $signed_user_id --role "Key Vault Administrator" --scope $key_vault_resource_id --output none
+    if [ $? -eq 0 ]; then
+        echo "Key Vault Administrator role assigned successfully."
+    else
+        echo "Failed to assign Key Vault Administrator role."
+        exit 1
+    fi
+else
+    echo "User already has the Key Vault Administrator role."
+fi
+
+echo "Getting Azure SQL Server resource id"
+sql_server_resource_id=$(az sql server show --name $sqlServerName --resource-group $resourceGroupName --query id --output tsv)
+
+# Check if the user is Azure SQL Server Admin
+echo "Checking if user is Azure SQL Server Admin"
+admin=$(MSYS_NO_PATHCONV=1 az sql server ad-admin list --ids $sql_server_resource_id --query "[?sid == '$signed_user_id']" -o tsv)
+
+# Check if the role exists
+if [ -n "$admin" ]; then
+    echo "User is already Azure SQL Server Admin"
+else
+    echo "User is not Azure SQL Server Admin. Assigning the role."
+    MSYS_NO_PATHCONV=1 az sql server ad-admin create --display-name "$signed_user_display_name" --object-id $signed_user_id --resource-group $resourceGroupName --server $sqlServerName --output none
+    if [ $? -eq 0 ]; then
+        echo "Assigned user as Azure SQL Server Admin."
+    else
+        echo "Failed to assign Azure SQL Server Admin role."
+        exit 1
+    fi
+fi
 
 # RUN apt-get update
 # RUN apt-get install python3 python3-dev g++ unixodbc-dev unixodbc libpq-dev
@@ -21,18 +84,74 @@ curl --output "create_sql_tables.py" ${baseUrl}"infra/scripts/index_scripts/crea
 # # RUN apt-get install python3 python3-dev g++ unixodbc-dev unixodbc libpq-dev
 # pip install pyodbc
 
-# Download the requirement file
-curl --output "$requirementFile" "$requirementFileUrl"
+pythonScriptPath="infra/scripts/index_scripts/"
 
-echo "Download completed"
+# Check if running in Azure Container App
+if !([ -z "$baseUrl" ] && [ -z "$managedIdentityClientId" ]); then
+    requirementFile="requirements.txt"
+    requirementFileUrl=${baseUrl}${pythonScriptPath}"requirements.txt"
+
+    # Download the create_index and create table python files
+    curl --output "create_search_index.py" ${baseUrl}${pythonScriptPath}"create_search_index.py"
+    curl --output "create_sql_tables.py" ${baseUrl}${pythonScriptPath}"create_sql_tables.py"
+
+    # Download the requirement file
+    curl --output "$requirementFile" "$requirementFileUrl"
+
+    pythonScriptPath=""
+
+    echo "Download completed"
+
+fi
+
 
 #Replace key vault name 
-sed -i "s/kv_to-be-replaced/${keyvaultName}/g" "create_search_index.py"
-sed -i "s/mici_to-be-replaced/${managedIdentityClientId}/g" "create_search_index.py"
-sed -i "s/kv_to-be-replaced/${keyvaultName}/g" "create_sql_tables.py"
-sed -i "s/mici_to-be-replaced/${managedIdentityClientId}/g" "create_sql_tables.py"
+sed -i "s/kv_to-be-replaced/${keyvaultName}/g" ${pythonScriptPath}"create_search_index.py"
+sed -i "s/kv_to-be-replaced/${keyvaultName}/g" ${pythonScriptPath}"create_sql_tables.py"
+if [ -n "$managedIdentityClientId" ]; then
+    sed -i "s/mici_to-be-replaced/${managedIdentityClientId}/g" ${pythonScriptPath}"create_search_index.py"
+    sed -i "s/mici_to-be-replaced/${managedIdentityClientId}/g" ${pythonScriptPath}"create_sql_tables.py"
+fi
 
-pip install -r requirements.txt
+# create virtual environment
+# Check if the virtual environment already exists
+if [ -d $pythonScriptPath"scriptenv" ]; then
+    echo "Virtual environment already exists. Skipping creation."
+else
+    echo "Creating virtual environment"
+    python3 -m venv $pythonScriptPath"scriptenv"
+fi
 
-python create_search_index.py
-python create_sql_tables.py
+# handling virtual environment activation for different OS
+activate_env_output=$(source $pythonScriptPath"scriptenv/bin/activate" 2>&1)
+if [ -n "$activate_env_output" ]; then
+    source $pythonScriptPath"scriptenv/Scripts/activate"
+fi
+
+# Install the requirements
+echo "Installing requirements"
+pip install --quiet -r ${pythonScriptPath}requirements.txt
+echo "Requirements installed"
+
+error_flag=false
+echo "Running the python scripts"
+echo "Creating the search index and adding the data to the index"
+python $pythonScriptPath"create_search_index.py"
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to create search index."
+    error_flag=true
+fi
+
+echo "Creating the SQL tables and adding the data to the tables"
+python $pythonScriptPath"create_sql_tables.py"
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to create SQL tables."
+    error_flag=true
+fi
+
+if [ "$error_flag" = true ]; then
+    echo "One or more errors occurred during the script execution."
+    exit 1
+fi
+
+echo "Script completed"
