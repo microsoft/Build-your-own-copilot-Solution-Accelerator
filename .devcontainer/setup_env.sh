56663e2534d25a8750ca88ac926e1fd2ad5898f1
DB Lee <donlee@microsoft.com>
2025-04-16 10:57:18 -0700

    fix devcontainer.json and setup script to support initializing / node and function dev env.

--- .devcontainer/setup_env.sh
+++ .devcontainer/setup_env.sh
@@ -19,3 +19,10 @@ if ! command -v sqlcmd &> /dev/null; then
     echo "sqlcmd is not available in the PATH. Please check the installation."
     exit 1
 fi
+
+# Install Azure function core tool
+wget -q https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+
+sudo apt-get update
+sudo apt-get install azure-functions-core-tools-4
