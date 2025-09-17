ab930de2901ea3ec57883c6f6eabe6cd8b22956a
UtkarshMishra-Microsoft <v-utkamishra@microsoft.com>
2025-09-17 13:01:08 +0530

    UpdatedFile

--- src/frontend/src/api/api.ts
+++ src/frontend/src/api/api.ts
@@ -55,6 +55,7 @@ export const documentSectionGenerate = async (researchTopic: string, documentSec
 
   // check for errors
   if (!response.ok) {
+    console.error('There was an issue fetching your data.')
     return response
   }
 
