090534e1bbb589439c4580823c7c4137a36027a6
Niraj Chaudhari (Persistent Systems Inc) <v-nirajcha@microsoft.com>
2025-10-09 21:53:48 +0530

    fix script 1

--- infra/scripts/process_sample_data.sh
+++ infra/scripts/process_sample_data.sh
@@ -330,6 +330,12 @@ if az deployment group show --resource-group "$resourceGroupName" --name "$deplo
 			--query "properties.outputs.managedidentitY_WEBAPP_NAME.value" -o tsv)
 		echo "Web App Managed Identity Display Name (from outputs): $webAppManagedIdentityDisplayName"
 
+		webAppManagedIdentityClientId=$(az deployment group show \
+			--name "$deploymentName" \
+			--resource-group "$resourceGroupName" \
+			--query "properties.outputs.managedidentitY_WEBAPP_CLIENTID.value" -o tsv)
+		echo "Web App Managed Identity Client ID (from outputs): $webAppManagedIdentityClientId"
+
 		SqlDatabaseName=$(az deployment group show \
 			--name "$deploymentName" \
 			--resource-group "$resourceGroupName" \
@@ -378,6 +384,8 @@ else
     read -rp "Enter Key Vault Name: " keyvaultName
 	read -rp "Enter Web App Managed Identity Display Name: " webAppManagedIdentityDisplayName
 	read -rp "Enter Web App Managed Identity Client ID: " webAppManagedIdentityClientId
+	read -rp "Enter SQL Managed Identity Display Name: " sqlManagedIdentityDisplayName
+	read -rp "Enter SQL Managed Identity Client ID: " sqlManagedIdentityClientId
     read -rp "Enter AI Search Service Name: " aiSearchName
     read -rp "Enter AI Foundry Resource ID: " aif_resource_id
     read -rp "Enter Azure Subscription ID: " azSubscriptionId
